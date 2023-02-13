8957d946db0d9dad665f496d57b92d05f41d193b
Glitterz001 <pobadmus@gmail.com>
2023-02-13 22:55:52 +0000

    my second commit

--- models/state.py
+++ models/state.py
@@ -1,32 +1,12 @@
 #!/usr/bin/python3
-"""
-State Class from Models Module
-"""
-import os
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
+"""Defines the State class."""
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """State class handles all application states"""
-    if STORAGE_TYPE == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
-    else:
-        name = ''
+class State(BaseModel):
+    """Represent a state.
+    Attributes:
+        name (str): The name of the state.
+    """
 
-        @property
-        def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            city_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    name = ""
